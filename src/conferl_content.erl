25ed1f224cb12b82dd8dcd182143e116f9ae2460
David Cao <david_bluetrain@hotmail.com>
2015-04-10 18:32:06 -0300

    [#3]  Adding gets & sets to content.

--- src/conferl_content.erl
+++ src/conferl_content.erl
@@ -14,22 +14,60 @@
 -module(conferl_content).
 -author('david.cao@inakanetworks.com').
 
+
+-type message() ::
+        #{
+           id      => integer(),
+           message => string(),
+           user    => integer()
+           %date
+         }.
+
+
+-export_type( [message/0]).
+
 -type content() ::
         #{
            id      => integer(),
-           message => [message],
-           user    => string()
+           messages => [message()],
+           user    => integer()
            %date
          }.
 
 -export_type( [content/0]).
+%%% sumo_db callbacks
+-export([sumo_schema/0, sumo_wakeup/1, sumo_sleep/1]).
 
--export([ register_content/1
+-export([   new/3
+          , register_content/1
           , unregister_content/1
           , fetch_content/1
           , list_contents/1 ]).
 
 -behavior(sumo_doc).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% BEHAVIOUR CALLBACKS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% @doc Part of the sumo_doc behavior.
+%%
+
+-spec sumo_wakeup(sumo:doc()) -> content().
+sumo_wakeup(Data) ->  Data.
+
+%% @doc Part of the sumo_doc behavior.
+-spec sumo_sleep(content()) -> sumo:doc().
+sumo_sleep(Content) ->  Content.
+
+%% @doc Part of the sumo_doc behavior.
+-spec sumo_schema() -> sumo:schema().
+sumo_schema() ->
+    sumo:new_schema(?MODULE, [
+    sumo:new_field(id  , integer,       [not_null, auto_increment, id]),
+    sumo:new_field(user, integer,       [not_null]),
+    sumo:new_field(message_id, integer, [index])
+  ]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Public Api
@@ -37,8 +75,45 @@
 %%
 %% @doc functions definitions for content
 
+
+-spec new( integer(),[message()], integer()) -> content().
+new( Id , Messages, User ) -> 
+        #{
+           id       => Id,
+           messages => Messages,
+           user     => User
+           %date
+         }.
+
+
+%% Getters/Setters
+-spec id(content()) -> integer().
+id(Content) ->
+  maps:get(id, Content).
+
+-spec id(content(), integer()) -> content().
+id(Content, Id) ->
+  Content#{id => Id} .
+
+-spec messages(content()) -> [message()].
+messages(Content) ->
+ maps:get(messages, Content).
+
+ -spec messages(content(), [message()]) -> content().
+messages(Content, Messages) ->
+ Content#{messages => Messages}.
+
+-spec user(content()) -> integer().
+user(Content) -> 
+  maps:get(user, Content).
+
+-spec user(content(), integer()) -> content().
+user(Content, User) -> 
+  Content#{user => User}.  
+
 -spec register_content(Url :: iodata()) -> 
   conferl_contents:content() | error.
+
 
 register_content(Url) -> #{}.
 %% todo
@@ -56,25 +131,3 @@ fetch_content(ContentId) ->   notfound.
 
 -spec list_contents(Domain :: iodata()) -> [conferl_contents:content()].
 list_contents(Domain) -> [ #{} ].
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% BEHAVIOUR CALLBACKS
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% @doc Part of the sumo_doc behavior.
--spec sumo_wakeup(sumo:doc()) -> content().
-sumo_wakeup(Data) ->  Data.
-
-%% @doc Part of the sumo_doc behavior.
--spec sumo_sleep(content()) -> sumo:doc().
-sumo_sleep(Content) ->  Content.
-
-%% @doc Part of the sumo_doc behavior.
--spec sumo_schema() -> sumo:schema().
-sumo_schema() ->
-    sumo:new_schema(?MODULE, [
-    sumo:new_field(id, integer,         [not_null, auto_increment, id]),
-    sumo:new_field(user, string,        [{length, 128}, not_null, unique]),
-    sumo:new_field(message_id, integer, [index])
-  ]).
-%% todo
--- src/conferl_content_repo.erl
+++ src/conferl_content_repo.erl
@@ -1,20 +1,37 @@
 -module(conferl_content_repo).
 -author('david.cao@inakanetworks.com').
 
--behaviour(sumo_repo).
-
 %%% General repo functions.
 -export(
-  [ init/1
-  , create_schema/2
+  [ create/3
+  , update/1
   , delete/3
   , delete_all/2
   , delete_by/3
-  , execute/2
-  , execute/3
   , find_all/2
   , find_all/5
   , find_by/3
   , find_by/5
   , persist/2
   ]).
+
+
+-spec create( Id       :: integer()
+            , Messages :: [message]
+            , User     :: integer()) -> conferl_content:content().
+create(  Id  ,Messages ,User ) ->
+  Content = conferl_content:new( Id ,Messages , User),
+  sumo:persist(conferl_content, Content).
+
+
+-spec update(conferl_content:content()) -> conferl_content:content().
+update( Content ) ->
+  sumo:persist(conferl_content, Content).
+
+-spec delete( conferl_content:content() ) -> integer().
+delete( Content ) -> 
+  Id = conferl_content:id(Content),
+  sumo:delete_by(conferl_content, [{id, Id}]).
+
+-spec delete_all() -> integer(). 
+delete_all() -> sumo:delete(conferl_content, Content). 

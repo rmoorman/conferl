201b45aa948b3182e5a7371951681a4e48073016
David Cao <david_bluetrain@hotmail.com>
2015-05-07 15:30:01 -0300

    [#20] Fixed Elvis trailing whitespace characters warnnings in handlers

--- src/handlers/cnf_contents_handler.erl
+++ src/handlers/cnf_contents_handler.erl
@@ -2,7 +2,7 @@
 % Version 2.0 (the "License"); you may not use this file
 % except in compliance with the License.  You may obtain
 % a copy of the License at
-% 
+%
 % http://www.apache.org/licenses/LICENSE-2.0
 %
 % Unless required by applicable law or agreed to in writing,
@@ -36,7 +36,12 @@
         ]).
 
 allowed_methods(Req, State) ->
-  {[<<"POST">>,<<"GET">>,<<"DELETE">>, <<"OPTIONS">>], Req, State}.
+  {[ <<"POST">>
+   , <<"GET">>
+   , <<"DELETE">>
+   , <<"OPTIONS">>]
+  , Req
+  , State}.
 
 handle_post(Req, State) ->
     lager:info("handle_get"),
@@ -44,7 +49,7 @@ handle_post(Req, State) ->
     Body = jiffy:decode(JsonBody, [return_maps]),
     lager:info("body ~p", [Body]),
     #{<<"url">> := Url, <<"user_id">> := UserId} = Body,
-    try 
+    try
       Content = cnf_content_repo:register(binary_to_list(Url), UserId),
       Id = cnf_content:id(Content),
       {Host, Req2} = cowboy_req:url(Req1),
@@ -56,35 +61,37 @@ handle_post(Req, State) ->
       throw:Exception ->
         cnf_utils:handle_exception(Exception, Req, State);
       Type:Exception ->
-        cnf_utils:handle_exception({Type,Exception}, Req, State)
+        cnf_utils:handle_exception({Type, Exception}, Req, State)
     end.
 
 handle_get(Req, State) ->
   lager:debug("handle_post"),
   {QsVal, Req2} =  cowboy_req:qs_val(<<"domain">>, Req),
   lager:info("QsVal ~p", [QsVal]),
-  case QsVal of 
-    undefined      -> {Id, Req3} = cowboy_req:binding(id_content, Req2), 
+  case QsVal of
+    undefined      -> {Id, Req3} = cowboy_req:binding(id_content, Req2),
       lager:info("get_resource - id_content ~p", [Id]),
-      RequestContent = cnf_content_repo:find(list_to_integer(binary_to_list(Id))),
+         =
+        cnf_content_repo:find(list_to_integer(binary_to_list(Id))),
       lager:info("RequestContent ~p", [RequestContent]),
       Body =  jiffy:encode(RequestContent),
       lager:info("Body ~p", [Body]),
       {Body, Req3, State};
-    Query  -> RequestContent = cnf_content_repo:find_by_domain(binary_to_list(Query)),
+    Query  -> RequestContent =
+      cnf_content_repo:find_by_domain(binary_to_list(Query)),
       lager:info("RequestContent qs ~p", [RequestContent]),
       Body =  jiffy:encode(RequestContent),
       lager:info("Body ~p", [Body]),
       {Body, Req2, State}
     end.
-    
 
 
 
-delete_resource(Req, State) ->  
-  {Id, Req1} = cowboy_req:binding(id_content, Req),  
+
+delete_resource(Req, State) ->
+  {Id, Req1} = cowboy_req:binding(id_content, Req),
   lager:info("delete_resource - id_content ~p", [Id]),
-  cnf_content_repo:unregister(list_to_integer(binary_to_list(Id))),  
+  cnf_content_repo:unregister(list_to_integer(binary_to_list(Id))),
   {true, Req1, State}.
 
 terminate(_Reason, _Req, _State) ->
--- src/handlers/cnf_status_handler.erl
+++ src/handlers/cnf_status_handler.erl
@@ -2,7 +2,7 @@
 % Version 2.0 (the "License"); you may not use this file
 % except in compliance with the License.  You may obtain
 % a copy of the License at
-% 
+%
 % http://www.apache.org/licenses/LICENSE-2.0
 %
 % Unless required by applicable law or agreed to in writing,
@@ -24,7 +24,7 @@
 init({tcp, http}, Req, _Opts) ->
   {ok, Req, undefined}.
 
-handle(Req, State) -> 
+handle(Req, State) ->
   {ok, Req2} =
     cowboy_req:reply(200
                     , [{<<"content-type">>, <<"text/plain">>}]
